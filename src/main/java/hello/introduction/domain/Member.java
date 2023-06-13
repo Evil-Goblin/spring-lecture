c04443aae607309ca3d3a977ec3c84bb4a4a4dd5
ForExample <jyh6197@gmail.com>
2023-06-14 00:35:37 +0900

    feat: Add Member , MemberRepository

---
+++ src/main/java/hello/introduction/domain/Member.java
@@ -0,0 +1,10 @@
+package hello.introduction.domain;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/hello/introduction/repository/MemoryMemberRepository.java
@@ -0,0 +1,40 @@
+package hello.introduction.repository;
+
+import hello.introduction.domain.Member;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private final Map<Long, Member> store = new ConcurrentHashMap<>();
+    private final AtomicLong sequence = new AtomicLong();
+
+    @Override
+    public Member save(Member member) {
+        member.setId(sequence.incrementAndGet());
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
