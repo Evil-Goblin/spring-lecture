463ec99d005381e23933347d258c0c05d9f6ae76
ForExample <jyh6197@gmail.com>
2023-07-23 12:20:04 +0900

    feat: Implement Decorator Chain using DecoratorPattern

--- proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -3,6 +3,7 @@ package hello.proxy.pureproxy.decorator;
 import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
 import hello.proxy.pureproxy.decorator.code.MessageDecorator;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.TimeDecorator;
 import org.junit.jupiter.api.Test;
 
 public class DecoratorPatternTest {
@@ -20,6 +21,16 @@ public class DecoratorPatternTest {
         RealComponent realComponent = new RealComponent();
         MessageDecorator messageDecorator = new MessageDecorator(realComponent);
         DecoratorPatternClient decoratorPatternClient = new DecoratorPatternClient(messageDecorator);
+
+        decoratorPatternClient.execute();
+    }
+
+    @Test
+    void decorator2() {
+        RealComponent realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        TimeDecorator timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient decoratorPatternClient = new DecoratorPatternClient(timeDecorator);
 
         decoratorPatternClient.execute();
     }
