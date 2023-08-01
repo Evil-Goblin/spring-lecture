7ac55b8c10830fb7382adbfb0b0858fc818e17e5
ForExample <jyh6197@gmail.com>
2023-08-01 20:46:11 +0900

    feat: Basic Bean Test

---
+++ proxy/src/test/java/hello/proxy/postprocessor/BasicTest.java
@@ -0,0 +1,46 @@
+package hello.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BasicTest {
+
+    @Test
+    void basicConfig() {
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(BasicConfig.class);
+
+        A beanA = annotationConfigApplicationContext.getBean("BeanA", A.class);
+        beanA.helloA();
+
+        Assertions.assertThatThrownBy(() -> annotationConfigApplicationContext.getBean("BeanB"))
+                .isInstanceOf(NoSuchBeanDefinitionException.class);
+
+    }
+
+    @Configuration
+    static class BasicConfig {
+        @Bean(name = "BeanA")
+        public A a() {
+            return new A();
+        }
+    }
+
+    @Slf4j
+    static class A {
+        public void helloA() {
+            log.info("hello A");
+        }
+    }
+
+    @Slf4j
+    static class B {
+        public void helloB() {
+            log.info("hello B");
+        }
+    }
+}
