28f541b7dca244b520975ce6db8ec62feb723e38
ForExample <jyh6197@gmail.com>
2023-08-06 18:40:56 +0900

    feat: Apply Aop using pointcut signature

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -2,7 +2,7 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV1;
+import hello.aop.order.aop.AspectV2;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@Import(AspectV1.class)
+@Import(AspectV2.class)
 @Slf4j
 @SpringBootTest
 public class AopTest {
