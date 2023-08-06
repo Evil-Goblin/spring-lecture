ef1d7f4eab456c8c8825c6b284523897dc4b8cb7
ForExample <jyh6197@gmail.com>
2023-08-06 16:07:38 +0900

    feat: Apply Aop using Aspect annotation

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,5 +1,6 @@
 package hello.proxy;
 
+import hello.proxy.config.v3_proxyfactory.AopConfig;
 import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
@@ -15,7 +16,8 @@ import org.springframework.context.annotation.Import;
 //@Import(ProxyFactoryConfigV1.class)
 //@Import(ProxyFactoryConfigV2.class)
 //@Import(BeanPostProcessorConfig.class)
-@Import(AutoProxyConfig.class)
+//@Import(AutoProxyConfig.class)
+@Import(AopConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") //주의 // spring boot 3이상부터 @Controller 어노테이션이 있어야만 컨트롤러로 인식이 되기 때문에 빈의 수동 등록을 위해 컴포넌트 스캔 범위에서 제외한다.
 public class ProxyApplication {
 
