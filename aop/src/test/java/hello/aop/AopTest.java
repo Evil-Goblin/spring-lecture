232c5490e0b412b68113637bd176009406396c33
ForExample <jyh6197@gmail.com>
2023-08-06 18:57:08 +0900

    feat: Apply Aop detach pointcut to other class

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -2,7 +2,7 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4PointCut;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@Import(AspectV3.class)
+@Import(AspectV4PointCut.class)
 @Slf4j
 @SpringBootTest
 public class AopTest {
