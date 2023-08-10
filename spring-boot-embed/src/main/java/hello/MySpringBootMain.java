fe7adf0b13f4503370ec881a0326ee06a1d270e4
ForExample <jyh6197@gmail.com>
2023-08-10 20:49:34 +0900

    feat: Implement Simple Boot Runner

---
+++ spring-boot-embed/src/main/java/hello/MySpringBootMain.java
@@ -0,0 +1,13 @@
+package hello;
+
+import hello.boot.MySpringApplication;
+import hello.boot.MySpringBootAnnotation;
+
+@MySpringBootAnnotation
+public class MySpringBootMain {
+
+    public static void main(String[] args) {
+        System.out.println("MySpringBootMain.main");
+        MySpringApplication.run(MySpringBootMain.class, args);
+    }
+}
