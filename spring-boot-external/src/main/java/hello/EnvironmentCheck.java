ef8198d41bd806df43289ebd35a3b8a867de8a28
ForExample <jyh6197@gmail.com>
2023-08-11 19:31:27 +0900

    feat: Test Spring Environment

---
+++ spring-boot-external/src/main/java/hello/EnvironmentCheck.java
@@ -0,0 +1,30 @@
+package hello;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class EnvironmentCheck {
+
+    private final Environment environment;
+
+    public EnvironmentCheck(Environment environment) {
+        this.environment = environment;
+    }
+
+    @PostConstruct
+    public void init() {
+        String a = environment.getProperty("a");
+        String b = environment.getProperty("b");
+        String c = environment.getProperty("c");
+        String d = environment.getProperty("d");
+
+        log.info("env a={}", a);
+        log.info("env b={}", b);
+        log.info("env c={}", c);
+        log.info("env d={}", d);
+    }
+}
