b1d16ab1d1678aa9659c1282e35fb3964baa8020
ForExample <jyh6197@gmail.com>
2023-08-11 22:32:02 +0900

    feat: Use Env using ConfigurationProperties

--- spring-boot-external-read/src/main/java/hello/ExternalReadApplication.java
+++ spring-boot-external-read/src/main/java/hello/ExternalReadApplication.java
@@ -1,14 +1,19 @@
 package hello;
 
+import hello.config.MyDataSourceConfigV1;
 import hello.config.MyDataSourceEnvConfig;
 import hello.config.MyDataSourceValueConfig;
+import hello.datasource.MyDataSourcePropertiesV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Import;
 
 //@Import(MyDataSourceEnvConfig.class)
-@Import(MyDataSourceValueConfig.class)
+//@Import(MyDataSourceValueConfig.class)
+@Import(MyDataSourceConfigV1.class)
 @SpringBootApplication(scanBasePackages = "hello.datasource")
+@ConfigurationPropertiesScan({"hello"}/*스캔 범위*/) // @EnableConfigurationProperties(MyDataSourcePropertiesV1.class) 대신 사용 가능 (컴포턴트 스캔과 비슷)
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
