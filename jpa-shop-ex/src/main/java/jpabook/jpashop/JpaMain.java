26c1ef85df4223f0040200e4dbb9378887fdfc4e
ForExample <jyh6197@gmail.com>
2023-08-23 00:00:46 +0900

    feat: Project Initialize

---
+++ jpa-shop-ex/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        try {
+
+            Order order = entityManager.find(Order.class, 1L);
+            Long memberId = order.getMemberId();
+            Member member = entityManager.find(Member.class, memberId);
+
+
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        } finally {
+            entityManager.close();
+        }
+
+        entityManagerFactory.close();
+    }
+}
