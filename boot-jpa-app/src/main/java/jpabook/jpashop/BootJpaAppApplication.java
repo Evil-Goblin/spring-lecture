66d32ee372534d1659b673f6d59340c354b14275
ForExample <jyh6197@gmail.com>
2023-08-29 20:21:28 +0900

    feat: Project Initialize

---
+++ boot-jpa-app/src/main/java/jpabook/jpashop/BootJpaAppApplication.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootJpaAppApplication.class, args);
+    }
+
+}
