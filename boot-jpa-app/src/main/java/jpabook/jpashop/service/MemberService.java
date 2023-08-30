2715809f0f8f003df6257d019bbc289fe2a024c5
ForExample <jyh6197@gmail.com>
2023-08-30 21:58:57 +0900

    feat: Project BaseCode

---
+++ boot-jpa-app/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class MemberService {
+
+    private final MemberRepository repository;
+
+    @Transactional // 얘는 readOnly 가 false
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        repository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = repository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    public List<Member> findMembers() {
+        return repository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return repository.findOne(memberId);
+    }
+}
--- boot-jpa-app/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ boot-jpa-app/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop;
 
-import org.assertj.core.api.Assertions;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -24,14 +24,14 @@ class MemberRepositoryTest {
     @Rollback(value = false)
     public void testMember() {
         Member member = new Member();
-        member.setUsername("memberA");
+        member.setName("memberA");
 
-        Long savedId = repository.save(member);
+        repository.save(member);
 
-        Member findMember = repository.find(savedId);
+        Member findMember = repository.findOne(member.getId());
 
         assertThat(findMember.getId()).isEqualTo(member.getId());
-        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember.getName()).isEqualTo(member.getName());
 
         assertThat(findMember).isEqualTo(member);
     }
