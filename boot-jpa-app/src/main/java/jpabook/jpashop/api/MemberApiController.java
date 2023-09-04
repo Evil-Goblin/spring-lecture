6f1df432bc2336a097ced9cc337b68bf694ca645
ForExample <jyh6197@gmail.com>
2023-09-04 20:23:31 +0900

    feat: implement select api

--- boot-jpa-app/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ boot-jpa-app/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,11 +9,42 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @RestController
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers(); // entity 를 반환하는 방식은 좋지 않다.
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     @PostMapping("/api/v1/members") // entity 받지 않도록 설계하는 것이 좋다.
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
