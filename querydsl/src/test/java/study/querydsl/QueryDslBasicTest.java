70a7d09749cbde54958a7371712dc2298bcc10bc
ForExample <jyh6197@gmail.com>
2023-09-16 17:40:53 +0900

    feat: groupBy

--- querydsl/src/test/java/study/querydsl/QueryDslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -1,6 +1,7 @@
 package study.querydsl;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -17,6 +18,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
 
 @Transactional
 @SpringBootTest
@@ -36,9 +38,9 @@ public class QueryDslBasicTest {
         em.persist(teamB);
 
         Member memberA = new Member("memberA", 10, teamA);
-        Member memberB = new Member("memberB", 10, teamA);
-        Member memberC = new Member("memberC", 10, teamB);
-        Member memberD = new Member("memberD", 10, teamB);
+        Member memberB = new Member("memberB", 20, teamA);
+        Member memberC = new Member("memberC", 30, teamB);
+        Member memberD = new Member("memberD", 40, teamB);
 
         em.persist(memberA);
         em.persist(memberB);
@@ -91,7 +93,7 @@ public class QueryDslBasicTest {
     }
 
     @Test
-    public void resultFetch() {
+    void resultFetch() {
         List<Member> fetch = jpaQueryFactory
                 .selectFrom(member)
                 .fetch(); // list 조회
@@ -126,7 +128,7 @@ public class QueryDslBasicTest {
      * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
      */
     @Test
-    public void sort() {
+    void sort() {
         em.persist(new Member(null, 100));
         em.persist(new Member("MemberE", 100));
         em.persist(new Member("MemberF", 100));
@@ -171,5 +173,48 @@ public class QueryDslBasicTest {
         assertThat(queryResults.getLimit()).isEqualTo(2);
         assertThat(queryResults.getOffset()).isEqualTo(1);
         assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
+
+    @Test
+    void aggregation() {
+        List<Tuple> result = jpaQueryFactory
+                .select(
+                        member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min()
+                )
+                .from(member)
+                .fetch();
+
+        Tuple tuple = result.get(0);
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.max())).isEqualTo(40);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+    }
+
+    /**
+     * 팀의 이름과 각 팀의 평균 연령을 구하라.
+     */
+    @Test
+    void groupBy() {
+        List<Tuple> result = jpaQueryFactory
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name) // having 또한 가능하다.
+                .fetch();
+
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
     }
 }
