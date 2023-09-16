f3fd9dccde8dac9e40cb2c40196fc7ff619eec48
ForExample <jyh6197@gmail.com>
2023-09-17 00:39:07 +0900

    feat: SqlFunction

--- querydsl/src/test/java/study/querydsl/QueryDslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -671,4 +671,36 @@ public class QueryDslBasicTest {
                 .where(member.age.gt(18))
                 .execute();
     }
+
+    @Test
+    void sqlFunction() {
+        List<String> fetch = jpaQueryFactory
+                .select(
+                        Expressions.stringTemplate(
+                                "function('replace', {0}, {1}, {2})",
+                                member.username, "member", "M")
+                ).from(member)
+                .fetch();
+
+        for (String s : fetch) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    void sqlFunction2() {
+        List<String> fetch = jpaQueryFactory
+                .select(member.username)
+                .from(member)
+                .where(member.username.eq(
+//                        Expressions.stringTemplate("function('lower', {0})", member.username))
+                        member.username.lower()
+                        )
+                )
+                .fetch();
+
+        for (String s : fetch) {
+            System.out.println("s = " + s);
+        }
+    }
 }
