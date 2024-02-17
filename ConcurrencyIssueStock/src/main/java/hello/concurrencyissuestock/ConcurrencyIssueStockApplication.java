e9d0d6ca493cf2c775e3c57748a3c0c1d0fd9da9
ForExample <jyh6197@gmail.com>
2024-02-17 16:00:12 +0900

    feat: Project Initialize

---
+++ ConcurrencyIssueStock/src/main/java/hello/concurrencyissuestock/ConcurrencyIssueStockApplication.java
@@ -0,0 +1,13 @@
+package hello.concurrencyissuestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyIssueStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyIssueStockApplication.class, args);
+    }
+
+}
